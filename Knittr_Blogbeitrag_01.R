d09bc888d665e31c389c8f5986f8c486534ea1e8
unknown <rroesler@LAP-TOUCH1.iais.fraunhofer.de>
2012-11-20 18:25:12 +0100

    add some comments

--- Knittr_Blogbeitrag_01.R
+++ Knittr_Blogbeitrag_01.R
@@ -2,9 +2,9 @@ library(devtools)
 library(markdown)
 library(knitr)
 
-setwd("C:/Users/rroesler/R_Workspace/R_Blog/")
+setwd("C:/Users/rroesler/R_Workspace/R_Blog/") # Setzen des Pfades
 
-rmdFile <- "Blogbeitrag_MinimumWeightedBipartidMatching_01"
+rmdFile <- "Blogbeitrag_MinimumWeightedBipartidMatching_01" # Blogbeitrag
 knit2html(paste(rmdFile,".Rmd",sep=""))
 markdownToHTML(paste(rmdFile,".md",sep=""), 
                paste(rmdFile,".html",sep=""), fragment.only = TRUE)
